ab0cbbea42ae68b3b16d72d0ceb057ce26e5dddd
baechanyong1 <110149885+baechanyong1@users.noreply.github.com>
2023-06-16 12:54:29 +0900

    add index.js mongodb connect

---
+++ schemas/index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/SOLO_BLOG")
+    .then(() => {
+      console.log("몽고디비 연결 성공");
+    })
+    .catch((err) => console.log(err));
+};
+
+mongoose.connection.on("error", (err) => {
+  console.error("몽고디비 연결 에러", err);
+});
+
+module.exports = connect;
